7fc4a42b81c9af4b9267931670f489929654ef03
ChrisSensebe <christophe.sensebe@gmail.com>
2017-04-01 17:48:26 +0200

    updating check-list.ts

--- src/pages/check-list/check-list.ts
+++ src/pages/check-list/check-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, AlertController} from 'ionic-angular';
 
 /*
   Generated class for the CheckList page.
@@ -13,10 +13,78 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CheckListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  checkList: any;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public alertController: AlertController) {
+
+    this.checkList = navParams.get('checklist');
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CheckListPage');
   }
 
+  addItem(): void {
+    this.alertController.create({
+      title: 'Add item',
+      message: 'Enter the name of the task',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'cancel'
+        },
+        {
+          text: 'save',
+          handler: data => {
+            this.checkList.addItem(data.name);
+          }
+        }
+      ]
+    }).present();
+  }
+
+  toggleItem(item): void {
+    this.checkList.toggleItem(item);
+  }
+
+  removeItem(item): void {
+    this.checkList.removeItem(item);
+  }
+
+  renameItem(item): void {
+    this.alertController.create({
+      title: 'Rename item',
+      message: 'Enter the newname of the task',
+      inputs: [
+        {
+          name: 'name'
+        }
+      ],
+      buttons: [
+        {
+          text: 'cancel'
+        },
+        {
+          text: 'save',
+          handler: data => {
+            this.checkList.renameItem(item, data.name);
+          }
+        }
+      ]
+    }).present();
+  }
+
+  uncheckItems(): void {
+    this.checkList.items.forEach((item) => {
+      if(item.checked){
+        this.checkList.toggleItem(item);
+      }
+    });
+  }
 }
